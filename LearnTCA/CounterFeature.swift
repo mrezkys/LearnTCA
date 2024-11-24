4bcd7d03a0e3b8632c79f83b5c47a6c7828a2209
Muhammad Rezky <mrezkysulihin@gmail.com>
2024-11-24 19:11:49 +0700

    Network Request

--- LearnTCA/CounterFeature.swift
+++ LearnTCA/CounterFeature.swift
@@ -6,27 +6,48 @@
 //
 
 import ComposableArchitecture
+import Foundation
 
 @Reducer
 struct CounterFeature {
     @ObservableState
     struct State {
         var count = 0
+        var fact: String?
+        var isLoading: Bool = false
     }
     
     enum Action {
         case incrementButtonTapped
         case decrementButtonTapped
+        case factButtonTapped
+        case factResponse(String)
     }
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
             case .decrementButtonTapped:
+                state.fact = nil
                 state.count -= 1
                 return .none
             case .incrementButtonTapped:
+                state.fact = nil
                 state.count += 1
+                return .none
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared
+                        .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                }
+            case .factResponse(let response):
+                state.fact = response
+                state.isLoading = false
                 return .none
             }
         }
