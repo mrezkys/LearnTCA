33dbfeca11dc61e849f3a3f9e08c4157c6cbd36f
Muhammad Rezky <mrezkysulihin@gmail.com>
2024-11-24 17:01:08 +0700

    Initial Commit

---
+++ LearnTCA/LearnTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnTCAApp.swift
+//  LearnTCA
+//
+//  Created by Muhammad Rezky on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
